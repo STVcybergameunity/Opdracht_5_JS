685410caf18df237c1cc49b96782df9b93715a9c
STVcybergameunity <cybergameunity@gmail.com>
2026-03-03 14:38:05 +0100

    Leren tutorial

--- JavaScript/canvas.js
+++ JavaScript/canvas.js
@@ -1,14 +1,29 @@
-function startGame() {
-    myGameArea.start();
-}
+const canvas = document.getElementById('mainGame');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image()
+playerImage.src = 'Img/shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
 
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.context =
-        this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas,document.body.childNodes[0]);
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    // ctx.fillRect(50,50,100,100);
+    // ctx.drawImage(image, sourcex, sourcey, sourcew, sourceh, destinationx, destinationy, destinationw, destinationh);
+    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight,
+    spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    if (gameFrame % staggerFrames == 0){
+        if(frameX < 6) frameX ++;
+        else frameX = 0;
     }
+
+    gameFrame ++
+    requestAnimationFrame(animate);
 }
+animate();
