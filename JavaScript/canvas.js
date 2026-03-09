88a313e0c9185c38d8ef82a6d46e52505b1219b6
STVcybergameunity <cybergameunity@gmail.com>
2026-03-09 14:30:38 +0100

    Refactor code, hover effects, W now works

--- JavaScript/canvas.js
+++ JavaScript/canvas.js
@@ -1,44 +1,29 @@
+/**
+ * Asigning let variables
+ */
 let playerState = 'idle';
-let returnedValueFromKeydown
-let keyDownValue
-const arrayState = ['getHit', 'idle', 'jump', 'fall', 'run', 'dizzy', 'sit', 'roll', 'bite', 'ko']
-const dropdown = document.getElementById('animations');
-
-document.onkeydown = function(e){
-    keyDownValue = e.key;
-    setPlayerState(keyDownValue);
-}
-
-function setPlayerState(keyDownValue){
-    if (!isNaN(keyDownValue)){
-        returnedValueFromKeydown = Number(keyDownValue);
-        playerState = arrayState[returnedValueFromKeydown];
-        console.log(playerState)
-    }
-    else switch(keyDownValue) {
-        case 'w':
-            break;
-    }
-    
-}
-
-
-const canvas = document.getElementById('player');
-const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 600;
-const CANVAS_HEIGHT = canvas.height = 600;
-
-const playerImage = new Image()
-playerImage.src = 'Img/shadow_dog.png';
-const spriteWidth = 575;
-const spriteHeight = 523;
-
+let returnedValueFromKeydown;
+let keyDownValue;
 let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
-const staggerFrames = 5;
-const spriteAnimations = [];
-const animationStates = [
+
+/**
+ * Asigning const variables
+ */
+const _STATEGETHIT = 0;
+const _STATEJUMP = 2;
+const _SPRITEWIDTH = 575;
+const _SPRITEHEIGHT = 523;
+const _STAGGERFRAMES = 5;
+const _CANVAS = document.querySelector('#player');
+const _CTX = _CANVAS.getContext('2d');
+const _CANVAS_WIDTH = _CANVAS.width = 600;
+const _CANVAS_HEIGHT = _CANVAS.height = 600;
+const _PLAYERIMAGE = new Image()
+const _ARRAYSTATE = ['getHit', 'idle', 'jump', 'fall', 'run', 'dizzy', 'sit', 'roll', 'bite', 'ko'];
+const _SPRITEANIMATION = [];
+const _ANIMATIONSTATE = [
     {
         name: 'idle',
         frames: 7,
@@ -80,29 +65,63 @@ const animationStates = [
         frames: 4,
     },
 ];
-animationStates.forEach((state, index) => {
+
+/**
+ * Sets the playerstate to the value that was pressed
+ */
+document.onkeydown = function(e){
+
+    keyDownValue = e.key;
+    setPlayerState(keyDownValue);
+
+}
+
+function setPlayerState(keyDownValue){
+
+    if (!isNaN(keyDownValue)){
+
+        playerState = _ARRAYSTATE[keyDownValue];
+        console.log(playerState)
+
+    }
+
+    else switch(keyDownValue) {
+
+        case 'w':
+            playerState = _ARRAYSTATE[_STATEJUMP]
+            break;
+
+    } 
+}
+
+_PLAYERIMAGE.src = 'Img/shadow_dog.png';
+
+_ANIMATIONSTATE.forEach((state, index) => {
+
     let frames = {
         loc: [],
     }
+
     for (let j = 0; j < state.frames; j++){
-        let positionX = j * spriteWidth;
-        let positionY = index * spriteHeight;
+        let positionX = j * _SPRITEWIDTH;
+        let positionY = index * _SPRITEHEIGHT;
         frames.loc.push({x: positionX, y: positionY});
     }
-    console.log(index)
-    spriteAnimations[state.name] = frames;
+
+    _SPRITEANIMATION[state.name] = frames;
 });
-console.log(spriteAnimations);
 
 
 function animate(){
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
-    let frameX = spriteWidth * position;
-    let frameY = spriteAnimations[playerState].loc[position].y;
 
-    ctx.drawImage(playerImage, frameX, frameY,
-    spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    _CTX.clearRect(0, 0, _CANVAS_WIDTH, _CANVAS_HEIGHT);
+
+    let position = Math.floor(gameFrame/_STAGGERFRAMES) % _SPRITEANIMATION[playerState].loc.length;
+    let frameX = _SPRITEWIDTH * position;
+    let frameY = _SPRITEANIMATION[playerState].loc[position].y;
+
+    _CTX.drawImage(_PLAYERIMAGE, frameX, frameY,
+    _SPRITEWIDTH, _SPRITEHEIGHT, 0, 0, _SPRITEWIDTH, _SPRITEHEIGHT);
 
     gameFrame ++
     requestAnimationFrame(animate);
--- JavaScript/startbutton.js
+++ JavaScript/startbutton.js
@@ -1,5 +1,5 @@
 const element_borders = document.querySelector("main");
-const element_button = document.querySelector(".start");
+const element_button = document.querySelector("#start");
 
 element_button.addEventListener('click', startClick);
 
