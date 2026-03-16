b578e8259b18e053da30abf783cff1630bb9b49b
STVcybergameunity <cybergameunity@gmail.com>
2026-03-16 14:47:10 +0100

    Slight adjustments
    Changed around some things and added some coments

--- JavaScript/gamespeed.js
+++ JavaScript/gamespeed.js
@@ -1,3 +1,7 @@
+/******************************************
+ * Elements are taken from startbutton.js *
+ ******************************************/
+
 element_speed_button_fast.addEventListener('click', speedClick);
 element_speed_button_medium.addEventListener('click', speedClick);
 element_speed_button_slow.addEventListener('click', speedClick);
@@ -6,7 +10,7 @@ function amountOfEnemies(){
 
     for (let i = 0; i < numberOfEnemies; i++){
 
-    _ARRAY_OF_ENEMIES.push(new Enemy())
+        _ARRAY_OF_ENEMIES.push(new Enemy())
 
     }
 
@@ -14,25 +18,27 @@ function amountOfEnemies(){
 
 function speedClick(event){
 
-    console.log(event.target.innerText);
-
     switch(event.target.innerText) {
+
         case 'Medium':
             curveAmount = 8;
             numberOfEnemies = 10;
             amountOfEnemies();
             break;
+
         case 'Fast':
             curveAmount = 12;
             numberOfEnemies = 10;
             amountOfEnemies();
             break;
+
         case 'Slow':
         default:
             curveAmount = 4;
             numberOfEnemies = 5;
             amountOfEnemies();
             break;
+
     }
 
 }
--- JavaScript/hitdetection.js
+++ JavaScript/hitdetection.js
@@ -1,2 +1,6 @@
+/*********************
+ * Set the variables *
+ *********************/
+
 let rect1 = {x: 5, y:5, width: 50, height:50};
 let rect2 = {x: 20, y: 10, width: 10, height: 10};
