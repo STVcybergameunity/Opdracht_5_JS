a4b059e51ca338a5fad3fdcee965e616776d3c0d
STVcybergameunity <cybergameunity@gmail.com>
2026-03-17 12:02:39 +0100

    Added thing for hit detection and fixed buttons

--- JavaScript/gamespeed.js
+++ JavaScript/gamespeed.js
@@ -16,27 +16,44 @@ function amountOfEnemies(){
 
 }
 
+function hideElements(){
+
+    element_speed_button_fast.style.visibility = "hidden";
+    element_speed_button_medium.style.visibility = "hidden";
+    element_speed_button_slow.style.visibility = "hidden";
+
+}
+
 function speedClick(event){
 
     switch(event.target.innerText) {
 
         case 'Medium':
+
+            hideElements();
             curveAmount = 8;
             numberOfEnemies = 10;
             amountOfEnemies();
+
             break;
 
         case 'Fast':
+
+            hideElements();
             curveAmount = 12;
             numberOfEnemies = 10;
             amountOfEnemies();
+
             break;
 
         case 'Slow':
         default:
+
+            hideElements();
             curveAmount = 4;
             numberOfEnemies = 5;
             amountOfEnemies();
+
             break;
 
     }
